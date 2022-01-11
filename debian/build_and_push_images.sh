80f3760ab12036e3d4a89e7642631200f4662f7e
wangdapeng04 <wangdapeng04@baidu.com>
2022-01-11 15:13:51 +0800

    upload image to tinkerbell's registry
    tinkerbell is build by docker-compose

--- debian/build_and_push_images.sh
+++ debian/build_and_push_images.sh
@@ -7,8 +7,8 @@ fi
 
 apt-get -qq --yes --no-install-recommends install whois debootstrap apt-cacher-ng 2>/dev/null
 
-registry='10.4.4.2'
-proxy_url='http://10.4.4.2:3142'
+registry='8.8.8.8'
+proxy_url='http://8.8.8.8:3142'
 linux='debian'
 release='bullseye'
 variant='slim'
@@ -16,17 +16,17 @@ version='latest'
 origin=$linux:$release-$variant
 base=$release/base:$version
 
-docker build -t $registry/$release/base:$version 00-base/ --build-arg IMAGE=$origin --build-arg http_proxy=$proxy_url --build-arg https_proxy=$proxy_url
-docker push $registry/$release/base:$version
+docker build -t $registry:443/$release/base:$version 00-base/ --build-arg IMAGE=$origin --build-arg http_proxy=$proxy_url --build-arg https_proxy=$proxy_url
+docker push $registry:443/$release/base:$version
 
-docker build -t $registry/$release/disk-wipe:$version 01-disk-wipe/ --build-arg REGISTRY=$registry --build-arg IMAGE=$base
-docker push $registry/$release/disk-wipe:$version
+docker build -t $registry:443/$release/disk-wipe:$version 01-disk-wipe/ --build-arg REGISTRY=$registry:443 --build-arg IMAGE=$base
+docker push $registry:443/$release/disk-wipe:$version
 
-docker build -t $registry/$release/disk-partition:$version 02-disk-partition/ --build-arg REGISTRY=$registry --build-arg IMAGE=$base
-docker push $registry/$release/disk-partition:$version
+docker build -t $registry:443/$release/disk-partition:$version 02-disk-partition/ --build-arg REGISTRY=$registry:443 --build-arg IMAGE=$base
+docker push $registry:443/$release/disk-partition:$version
 
-docker build -t $registry/$release/install-root-fs:$version 03-install-root-fs/ --build-arg REGISTRY=$registry --build-arg IMAGE=$base
-docker push $registry/$release/install-root-fs:$version
+#docker build -t $registry/$release/install-root-fs:$version 03-install-root-fs/ --build-arg REGISTRY=$registry --build-arg IMAGE=$base
+#docker push $registry/$release/install-root-fs:$version
 
-docker build -t $registry/$release/cloud-init:$version 04-cloud-init/ --build-arg REGISTRY=$registry --build-arg IMAGE=$base
-docker push $registry/$release/cloud-init:$version
+#docker build -t $registry/$release/cloud-init:$version 04-cloud-init/ --build-arg REGISTRY=$registry --build-arg IMAGE=$base
+#docker push $registry/$release/cloud-init:$version
